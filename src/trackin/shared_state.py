568dfaf8380b06c41c2a3878d2f3a5f882126e68
mihaela_mihaylova <mihaela.b.mihaylova@gmail.com>
2024-09-24 10:10:06 +0200

    added file with variables

---
+++ src/trackin/shared_state.py
@@ -0,0 +1,16 @@
+# this files declares all the variables that are shared between widgets and utils
+# this allows for sharing this variables in both files and avoding circular imports
+
+# shared_state.py
+class SharedState:
+    def __init__(self):
+        self.track = []
+        self.track_dict = {}
+        self.G = None
+        self.DATA = []
+        self.TRACKED = False
+        self.MAX_SCORE = 1600
+        self.SCORE_FUNC = 'squared'
+        self.N_TRACKS = 0
+
+shared_state = SharedState()
