166ec25f69fa54335c9a70f3772b446aa5c1c57d
mihaela_mihaylova <mihaela.b.mihaylova@gmail.com>
2024-09-30 11:59:54 +0200

    updated shared_state

--- src/trackin/shared_state.py
+++ src/trackin/shared_state.py
@@ -6,6 +6,7 @@ class SharedState:
     def __init__(self):
         self.track = []
         self.track_dict = {}
+        self.track_lines = None
         self.G = None
         self.DATA = []
         self.TRACKED = False
