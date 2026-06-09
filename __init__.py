2865c8c9e9e77da334e8ccd06c67e9316f99e4f7
mihaela-mihaylova <90253303+mihaela-mihaylova@users.noreply.github.com>
2026-06-09 10:39:09 +0300

    changes in the tracking algorithm

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# __init__.py
+
+import napari
+
+from ._widget import initialize_viewer, trackin_main
+from .utils import build_graph  
+
+
+# Initialize the Napari viewer
+viewer = napari.Viewer()
+
+# Pass the viewer object to initialize the viewer in widgets
+initialize_viewer(viewer)
